c01b49225932e13e57109324c1cf108b1a7ae329
Jesus Laiz <zheileman@users.noreply.github.com>
2021-10-21 09:57:08 +0100

    Improve styling of the pagination controls
    To have better focus and hover status, in line with the design system.

--- app/assets/javascript/cse.js
+++ app/assets/javascript/cse.js
@@ -3,5 +3,10 @@ window.addEventListener("DOMNodeInserted", function() {
   // so we can override them to give the controls a more govuk appearance.
   document.querySelectorAll('input.gsc-input').forEach(function(e) {
     e.removeAttribute('style')
-  })
+  });
+
+  // Apply the `govuk-link` class to all links in the search results
+  document.querySelectorAll('.gsc-control-cse a, .gsc-control-cse [role="link"]').forEach(function(e) {
+    e.classList.add('govuk-link')
+  });
 });
